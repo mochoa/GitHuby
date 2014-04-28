30ef700f6ae3d519ed70bda6f50411162065f616
mochoa <mochoa@innsa.com>
2014-04-28 14:41:13 -0400

    Commit de Nueva Clase y modificación CLASE1

--- GitHubTest/GitHubTest/Class1.cs
+++ GitHubTest/GitHubTest/Class1.cs
@@ -20,5 +20,11 @@ namespace GitHubTest
             }
             return true;
         }
+
+        public bool Negative()
+        {
+            return false;
+            ;
+        }
     }
 }
